cbc62fb53ba36b0e0404ba9f389db49bd2e31345
Joffrey Van Laere <joffrey.vanlaere@oquila.be>
2024-10-28 21:14:06 +0100

    add api test project

---
+++ TestApi/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace TestApi.Extensions;
+
+internal static class ServiceCollectionExtensions
+{
+    public static void AddSqlConection(this IServiceCollection services, string connectionString)
+        => services.AddSingleton(new SqlConnection(connectionString));
+}
---
+++ TestApi/Program.cs
@@ -0,0 +1,36 @@
+using Dapper;
+using Microsoft.Data.SqlClient;
+using TestApi;
+using TestApi.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSqlConection(builder.Configuration["sql:connectionString"]!);
+var app = builder.Build();
+
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+QueryLoggerInterceptor.ConfigureLogger(logger);
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapGet("/", async (SqlConnection sqlConnection) =>
+{
+    var sqlQuery = "SELECT * FROM Apps";
+
+    await sqlConnection.QueryAsync(sqlQuery);
+})
+.WithOpenApi();
+
+await app.RunAsync();
